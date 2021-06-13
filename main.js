abc6b230d90d8b3864749f4d8993461afd4b35e8
Turan Ates <jamesdusky@dustytavern.com>
2021-06-13 19:26:52 -0400

    Debug

--- main.js
+++ main.js
@@ -202,7 +202,7 @@ var saved = 0;
 
 function today(){
   var weeks = 1;
-  var percent = 1;
+  var percent = 1.1;
   var temp = Math.round(list.length/(7*weeks));
   while(temp > (queries.available/percent)){
     temp = Math.round(list.length/(7*++weeks));
