eb86b0512e01e7b4a5d8221411629604fe8e41e4
Turan Ates <jamesdusky@dustytavern.com>
2021-06-19 14:49:47 -0400

    Debug

--- main.js
+++ main.js
@@ -147,7 +147,7 @@ async function update(param = 0){
   if(temp > list.length){
     temp = list.length;
   }
-  console.log(queries.available+" Searches available. Updating "+max+" users today \n#"+param+" to #"+temp);
+  console.log(queries.available+" Searches available. Updating #"+param+" to #"+temp);
   async function query(username, key, i){
     await queryApi(username, key).then((result) => {
       saveParam(i, 1);
