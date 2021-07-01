53661192281354e3394b8860c146f77ab95703b5
Turan Ates <jamesdusky@dustytavern.com>
2021-07-01 03:32:32 -0400

    Debug

--- scanner.js
+++ scanner.js
@@ -453,20 +453,24 @@ function cachePlayer(user){
         check.bio = JSON.stringify(check.bio);
         check.badge = JSON.stringify(check.badge);
         check.organization = JSON.stringify(Object.assign({}, check.organization));
+        /*
         const sql = "INSERT INTO `CACHE players` (event, cID, username, bio, badge, organization, avatar) VALUES ('First Entry', "+check.cID+", '"+check.username+"', ?, '"+check.badge+"', '"+check.organization+"', '"+check.avatar+"' );";
         con.query(sql, [check.bio], function (err, result, fields) {
           if(err) throw err;
         });
+        */
       }
       if(update){
         check.bio = JSON.stringify(check.bio);
         check.badge = JSON.stringify(check.badge);
         check.organization = JSON.stringify(Object.assign({}, check.organization));
         var eventString = eventUpdate.join(", ");
+        /*
         const sql = "INSERT INTO `CACHE players` (event, cID, username, bio, badge, organization, avatar) VALUES ('"+eventString+"', "+check.cID+", '"+check.username+"', ?, '"+check.badge+"', '"+check.organization+"', '"+check.avatar+"');";
         con.query(sql, [check.bio], function (err, result, fields) {
           if(err) throw err;
         });
+        */
       }
     });
   }
