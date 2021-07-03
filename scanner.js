be169e00ccce3db9a9d7abc8a76d0d94e12e9ad7
Turan Ates <jamesdusky@dustytavern.com>
2021-07-02 20:52:25 -0400

    Debug

--- scanner.js
+++ scanner.js
@@ -131,7 +131,10 @@ async function init(){
           console.log("[CRAWLER] - #"+(count+1)+" of #"+orgs[i].members+" | "+orgs[i].sid);
           console.log(count+" | "+(orgs[i].members-1));
           if(count == (orgs[i].members-1)){
-            saveParam(i, 3);
+            await saveParam(i, 3)
+            .then(()=>{
+              console.log(i+" saved");
+            })
             count = 0;
           }else{
             count++;
