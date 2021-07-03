3f2d17069c3094dc854ae8240a8a223f02a91953
Turan Ates <jamesdusky@dustytavern.com>
2021-07-02 23:33:40 -0400

    Debug

--- scanner.js
+++ scanner.js
@@ -129,9 +129,9 @@ async function init(){
           throw new Error(result.data);
         }else{
           console.log("[CRAWLER] - #"+(count+1)+" of #"+orgs[i].members+" | "+orgs[i].sid);
-          console.log(count+" | "+(orgs[i].members-1));
+          console.log(count+" | "+orgs[i].members);
           saveParam(i, 3);
-          if(count >= (orgs[i].members-1)){
+          if(count <= (orgs[i].members-1)){
             count = 0;
           }else{
             count++;
