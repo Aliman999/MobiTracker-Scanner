160271b3c79a0df1a15d1155a604da2aa45c9d75
Turan Ates <jamesdusky@dustytavern.com>
2021-07-04 17:54:04 -0400

    Debug

--- scanner.js
+++ scanner.js
@@ -257,8 +257,7 @@ getOrgs.getNewOrgs = async function(param){
     newOrgs = newOrgs.filter(onlyUnique);
     newOrgs.splice( newOrgs.indexOf("N/A"), 1);
     newOrgs.sort();
-
-    console.log(result);
+    console.log(newOrgs);
     for(var i = param; i < newOrgs.length; i++){
       console.log(i);
       orgLimiter.schedule({ id:newOrgs[i] }, scan, newOrgs[i], i)
