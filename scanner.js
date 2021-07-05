54bba8d988d712f2dee7e62b13432ca3dafa8f72
Turan Ates <jamesdusky@dustytavern.com>
2021-07-05 14:04:08 -0400

    Debug

--- scanner.js
+++ scanner.js
@@ -62,6 +62,7 @@ orgLimiter.on("failed", async (error, info) => {
 });
 
 orgLimiter.on("done", function(info){
+  console.log(info.args[2]+" | "+(newOrgs.length-1));
   if(intval(info.args[2]) == (newOrgs.length-1)){
     saveParam(0, 4);
     console.log("[SYSTEM] - Reached end of org list, restarting.");
