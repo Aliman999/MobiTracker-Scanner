fbcb70444082aed5ce6d1e960f034201b3178654
Turan Ates <jamesdusky@dustytavern.com>
2021-08-25 08:46:27 -0400

    Debug

--- scanner.js
+++ scanner.js
@@ -625,12 +625,17 @@ var saved = 0;
 function cachePlayer(user) {
   var download = function (uri, filename) {
     return new Promise(callback => {
-      request.head(uri, function (err, res, body) {
-        console.log('content-type:', res.headers['content-type']);
-        console.log('content-length:', res.headers['content-length']);
+      try{
+        request.head(uri, function (err, res, body) {
+          console.log('content-type:', res.headers['content-type']);
+          console.log('content-length:', res.headers['content-length']);
 
-        request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
-      });
+          request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+        });
+      }catch(error){
+        console.save(error);
+        callback();
+      }
     });
   };
   var update = false;
